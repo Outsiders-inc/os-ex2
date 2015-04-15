525e7a62b0ad13ea0492776fbb39a215df65c326
gilgvirts <gilgvirts@users.noreply.github.com>
2015-04-15 15:12:30 +0300

    Create thread.cpp

---
+++ thread.cpp
@@ -0,0 +1,74 @@
+/*
+ * thread.cpp
+ *
+ *  Created on: 15 March 2015
+ *      Author: David Gvirts, Shahar Osovsky
+ */
+
+#include "Thread.h"
+
+/**
+ * A constructor for Thread, receiving it's id as parameter.
+ */
+Thread::Thread(int tId, Priority priority, fptr f) :
+	_id(tId), _priority(priority),  _func(f) ,_numOfQuantums(0) , _state(READY) {
+}
+
+///**
+// * A copy constructor for Thread, receiving the source Thread as parameter.
+// */
+//Thread::Thread(Thread toCopy)
+//{
+//
+//}
+
+Thread::~Thread() {
+	// TODO Auto-generated destructor stub
+}
+
+/**
+ * Returns the number of quantums this thread ran (including the current).
+ */
+int Thread::getNumOfQuantums()
+{
+	return this->_numOfQuantums;
+}
+
+/**
+ * Increases the number of quantums this thread ran by one.
+ */
+void Thread::increaseQuantums()
+{
+	this->_numOfQuantums += 1;
+}
+
+/**
+ * Returns this thread's id;
+ */
+int Thread::getId()
+{
+	return this->_id;
+}
+
+/**
+ * Returns this thread's priority;
+ */
+Priority Thread::getPriority()
+{
+	return this->_priority;
+}
+
+/**
+ * Changes this thread's state to the one given as parameter.
+ */
+void Thread::changeState(State newState)
+{
+	this->_state = newState;
+}
+/**
+ *
+ */
+fptr Thread::getFunction()
+{
+	return this->_func;
+}
