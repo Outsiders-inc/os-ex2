a0bf5b40aee2531ac9062c76534a289ce556632b
gilgvirts <gilgvirts@users.noreply.github.com>
2015-04-15 15:13:41 +0300

    Create Itimer.h

---
+++ Itimer.h
@@ -0,0 +1,21 @@
+/*
+ * Itimer.h
+ *
+ *  Created on: Mar 19, 2015
+ *      Author: gilgvirts
+ */
+
+#ifndef ITIMER_H_
+#define ITIMER_H_
+
+#include <stdio.h>
+#include <signal.h>
+#include <sys/time.h>
+
+
+class Itimer{
+public:
+Itimer(int quanta, void (*func)(int));
+};
+
+#endif /* ITIMER_H_ */
